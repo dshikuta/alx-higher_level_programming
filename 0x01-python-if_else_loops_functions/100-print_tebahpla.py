66e1bb73787df84050efc5cab739f86b32ad3a2e
dshikuta <dshikuta@gmail.com>
2022-11-30 13:26:14 -0800

    print tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
-
-for index in range(0, 26):
-    word = ord('z') - index
-    if (index % 2 == 1):
-        word = chr(word - ord('Y') + ord('z'))
-    else:
-        word = chr(word)
-    print("{}".format(word), end='')
+#!/usr/bin/python
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
